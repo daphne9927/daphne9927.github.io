db9fa6ac977b2bb7b1435a9cf14741909eae1d8e
daphne9927 <52254223+daphne9927@users.noreply.github.com>
2019-07-30 19:14:09 -0500

    Update OpenWeatherMap.js

--- OpenWeatherMap.js
+++ OpenWeatherMap.js
@@ -16,7 +16,7 @@ function bindButtons(){
         document.getElementById('temperature').textContent = response.main.temp;
        // var currentTemp = response.main.temp;
        // if(currentTemp > 90){
-        //alert("It's too hot to walk the dog);
+        //alert("It's too hot to walk the dog");
          //     }
         document.getElementById('humidity').textContent = response.main.humidity;
       } else {
